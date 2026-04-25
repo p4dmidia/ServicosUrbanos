c8d3a454061d448143a5a680484cd8d769076529
P4D Midia <agenciap4d@gmail.com>
2026-04-24 23:24:36 -0300

    atualização + APP PWA 3

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,13 @@
-const CACHE_NAME = 'urbaship-v1';
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json'
-];
-
+// Simple Service Worker to satisfy PWA requirements
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  // Pass through all requests to the network
+  event.respondWith(fetch(event.request));
 });
